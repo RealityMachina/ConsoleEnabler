f58ced2fa0fe61f21ce10e27f9df1219800a8d79
RealityMachina <tomclancy1995@gmail.com>
2019-12-11 11:40:00 -0500

    Working commit.

---
+++ ConsoleEnabler/ConsoleEnable.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Harmony;
+using System.Reflection;
+using Base.Utils.GameConsole;
+
+namespace ConsoleEnabler
+{
+    public class ConsoleEnable
+    {
+        public static void Init()
+        {
+            var harmony = HarmonyInstance.Create("io.github.realitymachina.consoleenabler");
+            harmony.PatchAll(Assembly.GetExecutingAssembly());
+
+        }
+
+        [HarmonyPatch(typeof(GameConsoleWindow), "Awake")]
+        public static class PhoenixPoint_GameConsoeWindow_AWake_Patch
+        {
+            static void Postfix(GameConsoleWindow __instance)
+            {
+                 GameConsoleWindow.DisableConsoleAccess = false;
+                
+            }
+        }
+    }
+}
